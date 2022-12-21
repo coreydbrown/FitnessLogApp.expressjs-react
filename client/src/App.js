bd4bc3cd5f3c6a594d1e094a7e7c5a4cbabb6637
Corey Brown <corey.brown.ny@gmail.com>
2022-12-21 12:46:01 -0500

    Added Nav section (Appbar and Drawer) and Main page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import Landing from "./pages/Landing";
+import Nav from "./Nav";
+import Main from "./Main";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
-    <>
-      <Landing />
-    </>
+    <Box sx={{ display: "flex" }}>
+      <Nav />
+      <Main />
+    </Box>
   );
 }
 
