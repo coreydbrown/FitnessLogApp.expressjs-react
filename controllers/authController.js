065ec933c3987ad1453efbd6306f067af8349550
Corey Brown <corey.brown.ny@gmail.com>
2022-12-28 15:50:59 -0500

    Error-handling; Added error-handling for errors detected by mongoose, and also added error-handling in Register controller. Created custom error classes.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,19 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already exists");
+  }
+
+  const user = await User.create({ firstName, lastName, email, password });
   res.status(StatusCodes.OK).json({ user });
 };
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,27 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
+  console.log(err.statusCode);
+
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong, please try again later",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, please try again later",
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field must be unique)`;
+  }
+
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+  // res.status(defaultError.statusCode).json({ msg: err });
 };
 
 export default errorHandlerMiddleware;
