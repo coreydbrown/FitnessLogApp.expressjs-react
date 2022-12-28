717d82c7071c162416b6fd0dc04a46d18db6843a
Corey Brown <corey.brown.ny@gmail.com>
2022-12-28 11:52:14 -0500

    Created User model, installed mongoose and validator packages.

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please provide first name"],
+    minLength: 1,
+    maxLength: 30,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide last name"],
+    minLength: 1,
+    maxLength: 30,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email address",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minLength: 8,
+    maxLength: 30,
+  },
+});
+
+export default mongoose.Model("User", UserSchema);
